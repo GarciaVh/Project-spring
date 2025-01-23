8dc45ea93d11eea98ef7fc88aba29a24472e3528
vitor garcia <vitorh_mariano06@hotmail.com>
2025-01-22 23:30:06 -0300

    Add by id

--- src/main/java/com/devsuperior/dslist/services/GameServices.java
+++ src/main/java/com/devsuperior/dslist/services/GameServices.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,11 @@ public class GameServices {
     @Autowired
     private GameRepository gameRepository;
 
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
